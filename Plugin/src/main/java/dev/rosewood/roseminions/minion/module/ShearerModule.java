1055b0e57b27457dcb23ef076187543091290b28
Robin <rjmullineux@gmail.com>
2023-03-14 23:11:55 +0000

    Add 1.19.4 Support

--- Plugin/src/main/java/dev/rosewood/roseminions/minion/module/ShearerModule.java
+++ Plugin/src/main/java/dev/rosewood/roseminions/minion/module/ShearerModule.java
@@ -1,5 +1,8 @@
 package dev.rosewood.roseminions.minion.module;
 
+import dev.rosewood.guiframework.GuiFactory;
+import dev.rosewood.guiframework.gui.GuiSize;
+import dev.rosewood.guiframework.gui.screen.GuiScreen;
 import dev.rosewood.roseminions.minion.Minion;
 import dev.rosewood.roseminions.minion.setting.SettingAccessor;
 import dev.rosewood.roseminions.minion.setting.SettingSerializers;
@@ -23,10 +26,12 @@ public class ShearerModule extends MinionModule {
 
     public static final SettingAccessor<Integer> RADIUS;
     public static final SettingAccessor<Long> UPDATE_FREQUENCY;
+    public static final SettingAccessor<Integer> MAX_SHEEP;
 
     static {
         RADIUS = SettingsContainer.defineSetting(ShearerModule.class, SettingSerializers.INTEGER, "radius", 3, "The radius in which to shear sheep");
         UPDATE_FREQUENCY = SettingsContainer.defineSetting(ShearerModule.class, SettingSerializers.LONG, "update-frequency", 5000L, "How often sheep will be sheared (in milliseconds)");
+        MAX_SHEEP = SettingsContainer.defineSetting(ShearerModule.class, SettingSerializers.INTEGER, "max-sheep", 5, "The maximum number of sheep that can be sheared at once");
 
         SettingsContainer.redefineSetting(ShearerModule.class, MinionModule.GUI_TITLE, "Shearer Module");
         SettingsContainer.redefineSetting(ShearerModule.class, MinionModule.GUI_ICON, Material.SHEARS);
@@ -59,6 +64,8 @@ public class ShearerModule extends MinionModule {
         Predicate<Entity> predicate = entity -> entity.getType() == EntityType.SHEEP && !((Sheep) entity).isSheared();
         int radius = this.settings.get(RADIUS);
         this.minion.getWorld().getNearbyEntities(this.minion.getCenterLocation(), radius, radius, radius, predicate)
+                .stream()
+                .limit(this.settings.get(MAX_SHEEP))
                 .forEach(entity -> {
                     Sheep sheep = (Sheep) entity;
                     sheep.setSheared(true);
@@ -75,7 +82,15 @@ public class ShearerModule extends MinionModule {
 
     @Override
     protected void buildGui() {
+        this.guiContainer = GuiFactory.createContainer();
 
+        GuiScreen mainScreen = GuiFactory.createScreen(this.guiContainer, GuiSize.ROWS_THREE)
+                .setTitle(this.settings.get(MinionModule.GUI_TITLE));
+
+        this.addBackButton(mainScreen);
+
+        this.guiContainer.addScreen(mainScreen);
+        this.guiFramework.getGuiManager().registerGui(this.guiContainer);
     }
 
 
