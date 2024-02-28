ab1cf657785c15f100acc551e8529907e151f5af
Robin <oribuinn@gmail.com>
2024-02-28 16:34:03 +0000

    Update SkullUtils

--- Plugin/src/main/java/dev/rosewood/roseminions/util/nms/SkullUtils.java
+++ Plugin/src/main/java/dev/rosewood/roseminions/util/nms/SkullUtils.java
@@ -1,5 +1,4 @@
 package dev.rosewood.roseminions.util.nms;
-
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import dev.rosewood.rosegarden.utils.NMSUtil;
@@ -9,7 +8,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Base64;
 import java.util.UUID;
-import me.arcaniax.hdb.api.HeadDatabaseAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.profile.PlayerProfile;
@@ -30,18 +28,13 @@ public final class SkullUtils {
      * @param skullMeta The ItemMeta for the Skull
      * @param texture The texture to apply to the skull
      */
+    @SuppressWarnings("deprecation")
     public static void setSkullTexture(SkullMeta skullMeta, String texture) {
         if (texture == null || texture.isEmpty())
             return;
 
-        if (texture.startsWith("hdb:") && Bukkit.getPluginManager().isPluginEnabled("HeadDatabase")) {
-            texture = new HeadDatabaseAPI().getBase64(texture.substring(4));
-            if (texture == null)
-                return;
-        }
-
-        if (NMSUtil.getVersionNumber() >= 18) {
-            PlayerProfile profile = Bukkit.createPlayerProfile(UUID.randomUUID());
+        if (NMSUtil.getVersionNumber() >= 18) { // No need to use NMS on 1.18.1+
+            PlayerProfile profile = Bukkit.createPlayerProfile(UUID.nameUUIDFromBytes(texture.getBytes()));
             PlayerTextures textures = profile.getTextures();
 
             String decodedTextureJson = new String(Base64.getDecoder().decode(texture));
@@ -53,30 +46,30 @@ public final class SkullUtils {
                 e.printStackTrace();
             }
             skullMeta.setOwnerProfile(profile);
-        } else {
-            GameProfile profile = new GameProfile(UUID.nameUUIDFromBytes(texture.getBytes()), "");
-            profile.getProperties().put("textures", new Property("textures", texture));
+            return;
+        }
 
-            try {
-                if (NMSUtil.getVersionNumber() > 15) {
-                    if (method_SkullMeta_setProfile == null) {
-                        method_SkullMeta_setProfile = skullMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
-                        method_SkullMeta_setProfile.setAccessible(true);
-                    }
+        GameProfile profile = new GameProfile(UUID.nameUUIDFromBytes(texture.getBytes()), "");
+        profile.getProperties().put("textures", new Property("textures", texture));
 
-                    method_SkullMeta_setProfile.invoke(skullMeta, profile);
-                } else {
-                    if (field_SkullMeta_profile == null) {
-                        field_SkullMeta_profile = skullMeta.getClass().getDeclaredField("profile");
-                        field_SkullMeta_profile.setAccessible(true);
-                    }
-
-                    field_SkullMeta_profile.set(skullMeta, profile);
+        try {
+            if (NMSUtil.getVersionNumber() > 15) {
+                if (method_SkullMeta_setProfile == null) {
+                    method_SkullMeta_setProfile = skullMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+                    method_SkullMeta_setProfile.setAccessible(true);
                 }
-            } catch (ReflectiveOperationException e) {
-                e.printStackTrace();
+
+                method_SkullMeta_setProfile.invoke(skullMeta, profile);
+            } else {
+                if (field_SkullMeta_profile == null) {
+                    field_SkullMeta_profile = skullMeta.getClass().getDeclaredField("profile");
+                    field_SkullMeta_profile.setAccessible(true);
+                }
+
+                field_SkullMeta_profile.set(skullMeta, profile);
             }
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
         }
     }
-
 }
